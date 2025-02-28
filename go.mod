7585789f01621b1e097887db201216861c8c57dd
thenoobgrammer <thenoobgrammer@gmail.com>
2025-02-28 08:43:10 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module bitcask-db
+
+go 1.21.6
+
+require (
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"sync"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type BitcaskHandle *string
+
+type DataStructure struct {
+	tstamp timestamp.Timestamp
+	key    string
+	value  any
+}
+
+type BitcaskStore struct {
+	handler      string
+	mu           sync.Mutex
+	data         []DataStructure
+	last_opened  timestamp.Timestamp
+	last_updated timestamp.Timestamp
+}
+
+var store BitcaskStore = BitcaskStore{
+	handler: "dmnjkshoqwhorfhn2417801237@0y2941hqhw-$@480&(@)&$)HY*!(?)",
+}
+
+func main() {
+
+}
+
+func (s *BitcaskStore) open(dir_name string) BitcaskHandle {
+	return &store.handler
+}
+func (s *BitcaskStore) openWopts(dir_name string, opts map[string]interface{}) BitcaskHandle {
+	return nil
+}
+func (s *BitcaskStore) get(key string) any {
+	store.mu.Lock()
+	for _, d := range s.data {
+		if d.key == key {
+			store.mu.Unlock()
+			return &d
+		}
+	}
+	return nil
+}
+func (s *BitcaskStore) put(handler BitcaskHandle, key string, value any) {
+	store.mu.Lock()
+	s.data = append(s.data, DataStructure{
+		key:    key,
+		value:  value,
+		tstamp: *timestamppb.Now(),
+	})
+	store.mu.Unlock()
+}
