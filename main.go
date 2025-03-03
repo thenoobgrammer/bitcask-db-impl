1a3af5713732aac10bb47f73ec7b61dce3e5991d
Antoine Hakim <ahakim@ahakim-mbp.home>
2025-03-03 11:52:21 -0500

    updates

--- main.go
+++ main.go
@@ -6,32 +6,30 @@ import (
 	"path"
 	"path/filepath"
 	"runtime"
+	"sync"
 
 	"github.com/golang/protobuf/ptypes/timestamp"
-	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+var (
+	Mu sync.Mutex
 )
 
 type BitcaskHandle struct {
-	File  *os.File
-	Path  string
-	Index map[string]int64
+	File *os.File
+	Path string
+	// Index map[string]int64
 }
 
-type DataStructure struct {
+type FileData struct {
 	tstamp  timestamp.Timestamp
 	key     string
 	value   any
 	deleted bool
 }
 
-// type BitcaskStore struct {
-// 	Mu    sync.Mutex
-// 	DataFile BitcaskHandle
-// 	MetadataFile BitcaskHandle
-// }
-
 func main() {
-	handler := open("user_db")
+	open("user_db")
 }
 
 func rootDir() string {
@@ -39,7 +37,8 @@ func rootDir() string {
 	return path.Dir(b)
 }
 
-func open(dir_name string) BitcaskHandle {
+func open(dir_name string) *BitcaskHandle {
+	Mu.Lock()
 	_createDir(filepath.Base(dir_name))
 	data_file, err := os.Create(filepath.Join(filepath.Base(dir_name), "bitcask.data"))
 	if err != nil {
@@ -49,41 +48,11 @@ func open(dir_name string) BitcaskHandle {
 	if err != nil {
 		fmt.Printf("Metadata file failed to create: %s", err.Error())
 	}
+	Mu.Unlock()
 	return &BitcaskHandle{
 		File: data_file,
 		Path: dir_name,
-		Index: ,
 	}
-}
-func (s *BitcaskStore) openWopts(dir_name string, opts map[string]interface{}) BitcaskHandle {
-	return nil
-}
-func (s *BitcaskStore) get(key string) any {
-	store.mu.Lock()
-	idx := indexes[key]
-	store.mu.Unlock()
-	return s.data[idx]
-}
-func (s *BitcaskStore) put(handler BitcaskHandle, key string, value any) {
-	store.mu.Lock()
-	s.data = append(s.data, DataStructure{
-		key:    key,
-		value:  value,
-		tstamp: *timestamppb.Now(),
-	})
-	indexes[key] = len(s.data) - 1
-	store.mu.Unlock()
-}
-func (s *BitcaskStore) delete(handler BitcaskHandle, key string, value any) {
-	store.mu.Lock()
-	s.data = append(s.data, DataStructure{
-		key:     key,
-		value:   value,
-		tstamp:  *timestamppb.Now(),
-		deleted: true,
-	})
-	delete(indexes, key)
-	store.mu.Unlock()
 }
 
 func _createDir(name string) {
